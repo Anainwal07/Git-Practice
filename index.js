b11b351c47331001db38d476ede39baf865a8cf0
Ankit Nainwal <ankit.nainwal.3150@gmail.com>
2024-09-27 15:01:08 +0530

    Ask name function added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const myApp = () => {
     console.log("I'm fine Thankyou");
 }
-const Howru = () => {
+const Howudoin = () => {
     setTimeout(myApp , 3000) ;// It will run after 3 seconds 
 }
+const WhatIsName = () => {
+    setTimeout(askname , 3000) ; //It will run after 3 seconds 
+}
 
+console.log("What is your name");
 console.log("Hello How are you");
 
-
+WhatIsName() ;
 Howudoin() ;
